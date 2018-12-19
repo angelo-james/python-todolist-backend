a90166cee2f5064e6b135f312b49e590d56cd842
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-12-18 19:20:15 -0800

    created get route for all customers

--- db/db.py
+++ db/db.py
@@ -5,10 +5,27 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'mongotask'
-app.config['MONGO_URI'] = ''
+app.config['MONGO_DBNAME'] = 'runners_mind_python'
+app.config['MONGO_URI'] = 'mongodb://aj:aj123456@ds052819.mlab.com:52819/runners_mind_python'
 
 mongo = PyMongo(app)
 
 CORS(app)
 
+@app.route('/', methods=['GET'])
+def get_all_customers():
+      customers = mongo.db.customers
+      result = []
+
+      for field in customers.find():
+        result.append(
+          {
+            '_id': str(field['_id']),
+            'name': field['name'],
+            'address': field['address']
+            }
+        )
+      return jsonify(result)
+
+if __name__ == '__main__':
+  app.run(debug=True)
