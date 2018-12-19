e67f8a2247c38b4c9466d998bc53809270856281
Angelo James Arriola <angelojames.arriola@gmail.com>
2018-12-18 19:40:45 -0800

    changed endpoints to users created POST route to create a user

--- db/db.py
+++ db/db.py
@@ -12,20 +12,36 @@ mongo = PyMongo(app)
 
 CORS(app)
 
-@app.route('/', methods=['GET'])
+@app.route('/api/users', methods=['GET'])
 def get_all_customers():
-      customers = mongo.db.customers
-      result = []
+  users = mongo.db.users
+  result = []
 
-      for field in customers.find():
-        result.append(
-          {
-            '_id': str(field['_id']),
-            'name': field['name'],
-            'address': field['address']
-            }
-        )
-      return jsonify(result)
+  for field in users.find():
+    result.append(
+      {
+        '_id': str(field['_id']),
+        'name': field['name']
+      }
+    )
+  return jsonify(result)
+
+@app.route('/api/users', methods=['POST'])
+def add_customer():
+  users = mongo.db.users
+  name = request.get_json()['name']
+
+  name_id = users.insert(
+    {
+      'name': name
+    }
+  )
+
+  new_user = users.find_one({'_id': name_id})
+
+  result = {'title': new_user['name']}
+
+  return jsonify({'result': result})
 
 if __name__ == '__main__':
   app.run(debug=True)
